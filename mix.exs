6478170fa973d08dc3c42dd7d98ecfbc506d6e14
Manuel Rubio <manuel@altenwald.com>
2026-08-24 01:29:03 +0200

    ci: configure dialyzer PLT cache directory in .plts

--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule Passby.MixProject do
 
   defp dialyzer do
     [
+      plt_local_path: ".plts",
+      plt_core_path: ".plts",
       plt_add_apps: [:inets, :ssl, :public_key, :logger],
       flags: [:error_handling, :unknown]
     ]
